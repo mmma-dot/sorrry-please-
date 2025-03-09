9eb527a8ee95adc135a23bd8c66d5a9943befcbd
mmma-dot <urentsev05@mail.ru>
2025-03-09 05:47:27 +0500

    Update script.js

---
+++ sorry/script.js
@@ -0,0 +1,12 @@
+function showSurprise() {
+    document.body.innerHTML = 
+        <div class="container">
+            <h1 class="apology-text">Я люблю тебя больше всего на свете!</h1>
+            <p class="message">Ты самое дорогое, что у меня есть. Спасибо, что ты есть! ❤️</p>
+            <div class="heart"></div>
+            <a href="https://www.youtube.com/watch?v=d-diB65scQU" target="_blank">
+                <button>Для тебя 🎶</button>
+            </a>
+        </div>
+    ;
+}
